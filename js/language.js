f6a8b3a29b282a71a1958b95a74bc6f84f71c37c
homerofsteel <lucas.v85@outlook.com>
2025-11-02 14:36:22 +0100

    reformulation of multiple sentences

--- js/language.js
+++ js/language.js
@@ -4,14 +4,16 @@ const translations = {
     nav_skills: "Compétences",
     nav_projects: "Projets",
     nav_contact: "Contact",
-    hero_title: "Bonjour, moi c’est <span>Lucas Vernageau</span>",
+    hero_title: "Bonjour, je suis <span>Lucas Vernageau</span>",
     hero_text:
       "Étudiant en développement Web Full Stack.<br> Étudiant chez Ynov - Nantes",
     hero_btn: "Voir mes projets",
     about_title: "À propos de moi",
-    about_text: `Étudiant passionné par le développement web, j’aime créer des sites simples et efficaces qui répondent à une problèmatique concrète.
-           Je me forme actuellement au HTML, CSS et JavaScript, avec l’objectif de devenir développeur full stack.
-           Je suis aussi passioné par les jeux vidéos, la musique et j'aime acquérir de nouvelles connaissances en permanence.`,
+    about_text: `Étudiant passionné par le développement web, j’aime concevoir des sites simples et efficaces, pensés pour offrir une expérience utilisateur fluide et intuitive.
+
+Actuellement en formation à Ynov Nantes, je développe mes compétences en HTML, CSS, JavaScript et en outils front et back-end dans le but de devenir développeur full stack.
+
+Curieux et motivé, je m’intéresse également à la création de jeux vidéo, à la musique et à tout ce qui me permet d’explorer de nouvelles technologies et d’apprendre en continu.`,
     skills_title: "Mes <span>Compétences</span>",
     skills_subtitle:
       "Technologies et outils avec lesquels je travaille pour construire des solutions simples et efficaces",
@@ -28,8 +30,8 @@ const translations = {
     details: "Détails",
     eyetracker_description: "Un simple site qui suit ta souris.",
     pacman_description: "Version modifiée de Pac-Man.",
-    moreprojects: "Plus de projets",
-    contactdetails: "Intéressé par mon profil ? N’hésite pas à me contacter :",
+    moreprojects: "Voir plus de projets",
+    contactdetails: "Intéressé par mon profil ? N’hésitez pas à me contacter :",
     open: "Ouvrir",
     copyright: "© 2025 Lucas Vernageau",
   },
@@ -42,9 +44,11 @@ const translations = {
     hero_text: "Web development student.<br> Student at Ynov - Nantes",
     hero_btn: "View my projects",
     about_title: "About Me",
-    about_text: `A student passionate about web development, I love creating simple and efficient websites.
-           I am currently learning HTML, CSS, and JavaScript, with the goal of becoming a full-stack developer.
-           I am also passionate about video games, music, and love constantly acquiring new knowledge.`,
+    about_text: `I’m a passionate web development student who enjoys designing simple and effective websites focused on delivering a smooth and intuitive user experience.
+
+Currently studying at Ynov Nantes, I’m developing my skills in HTML, CSS, JavaScript, and both front-end and back-end technologies, with the goal of becoming a full-stack developer.
+
+Curious and motivated, I’m also passionate about video games, music, and exploring new technologies to keep learning and growing every day.`,
     skills_title: "My <span>Skills</span>",
     skills_subtitle:
       "Technologies and tools I work with to build simple and efficient solutions",
@@ -62,7 +66,7 @@ const translations = {
     details: "Details",
     eyetracker_description: "A simple site that tracks your mouse.",
     contactdetails: "Interested in my profile? Feel free to contact me:",
-    moreprojects: "More projects",
+    moreprojects: "See more projects",
     open: "Open",
     copyright: "© 2025 Lucas Vernageau",
   },
